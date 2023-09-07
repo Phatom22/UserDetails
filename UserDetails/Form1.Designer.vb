28cc9c5b992295caeb005c93946609938a6563e7
nomvula <nzshabangu95@gmail.com>
2023-09-07 03:48:11 +0200

    Add project files.

---
+++ UserDetails/Form1.Designer.vb
@@ -0,0 +1,164 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtSurname = New System.Windows.Forms.TextBox()
+        Me.txtYear = New System.Windows.Forms.TextBox()
+        Me.txtTribe = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lstGender = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(325, 64)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "name"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(492, 58)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 26)
+        Me.txtName.TabIndex = 1
+        '
+        'txtSurname
+        '
+        Me.txtSurname.Location = New System.Drawing.Point(492, 120)
+        Me.txtSurname.Name = "txtSurname"
+        Me.txtSurname.Size = New System.Drawing.Size(100, 26)
+        Me.txtSurname.TabIndex = 2
+        '
+        'txtYear
+        '
+        Me.txtYear.Location = New System.Drawing.Point(492, 182)
+        Me.txtYear.Name = "txtYear"
+        Me.txtYear.Size = New System.Drawing.Size(100, 26)
+        Me.txtYear.TabIndex = 3
+        '
+        'txtTribe
+        '
+        Me.txtTribe.Location = New System.Drawing.Point(492, 239)
+        Me.txtTribe.Name = "txtTribe"
+        Me.txtTribe.Size = New System.Drawing.Size(100, 26)
+        Me.txtTribe.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(319, 120)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(71, 20)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "surname"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(325, 182)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(92, 20)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "year of birth"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(323, 244)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(40, 20)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "tribe"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(316, 311)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(59, 20)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "gender"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(329, 382)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(210, 43)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "your small bio"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lstGender
+        '
+        Me.lstGender.FormattingEnabled = True
+        Me.lstGender.ItemHeight = 20
+        Me.lstGender.Items.AddRange(New Object() {"Male", "Female"})
+        Me.lstGender.Location = New System.Drawing.Point(492, 292)
+        Me.lstGender.Name = "lstGender"
+        Me.lstGender.Size = New System.Drawing.Size(120, 84)
+        Me.lstGender.TabIndex = 11
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lstGender)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtTribe)
+        Me.Controls.Add(Me.txtYear)
+        Me.Controls.Add(Me.txtSurname)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtSurname As TextBox
+    Friend WithEvents txtYear As TextBox
+    Friend WithEvents txtTribe As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lstGender As ListBox
+End Class
---
+++ UserDetails/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim name As String
+        Dim Surname As String
+        Dim age As Integer
+        Dim Tribe As String
+        Dim Greet As String
+        Dim Gender As String
+        Dim GenderDesc As String
+
+        name = txtName.Text
+        Surname = txtSurname.Text
+        age = txtYear.Text
+        Gender = lstGender.SelectedItem
+
+        Tribe = txtTribe.Text.ToLower
+        If Tribe = "tshivenda" Then
+            Greet = "ndaa"
+        ElseIf Tribe = "xhosa" Then
+            Greet = "molo"
+        ElseIf Tribe = "siswati" Then
+            Greet = "sawubona"
+        ElseIf Tribe = "sipedi" Then
+            Greet = "thobela"
+        ElseIf Tribe = "tswana" Then
+            Greet = "dumelang"
+        ElseIf Tribe = "zulu" Then
+            Greet = "sawubona"
+        ElseIf Tribe = "afrikaans" Then
+            Greet = "hallo"
+        ElseIf Tribe = "ndebele" Then
+            Greet = "lotjani"
+        ElseIf Tribe = "xitsonga" Then
+            Greet = "avuxeni"
+        ElseIf Tribe = "sisotho" Then
+            Greet = "dumela"
+        Else Greet = "hi"
+        End If
+
+
+        If Gender = "Male" Then
+            GenderDesc = "man"
+        ElseIf Gender = "Female" Then
+            GenderDesc = "lady"
+        Else GenderDesc = " please select your gender"
+        End If
+
+
+        MsgBox(Greet & " " & name & " " & Surname & " you are  " & 2023 - age & " years old " & GenderDesc & " who speaks " & Tribe)
+    End Sub
+End Class
